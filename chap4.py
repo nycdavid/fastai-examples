d7f2a9be2834c31d40aca914d0d58ebc613e7c7f
David Ko <davidsj.ko@gmail.com>
2022-05-09 16:43:21 -0400

    Stopping points

--- chap4.py
+++ chap4.py
@@ -1,9 +1,20 @@
-threes = []
+from fastai.vision.all import *
+from fastbook import *
 
-result = torch.tensor([])
-# stacking all threes
-for path in threes:
-  # open the path
-  # convert to a PyTorch tensor
-  img = Image.open(path)
-  tensor = torch.tensor(array(img))
+path = untar_data(URLs.MNIST_SAMPLE)
+threes = (path/'train'/'3').ls().sorted()
+sevens = (path/'train'/'7').ls().sorted()
+
+three_tensors = [tensor(Image.open(img_path)) for img_path in threes]
+seven_tensors = [tensor(Image.open(img_path)) for img_path in sevens]
+
+stacked_threes = torch.stack(three_tensors).float() / 255
+stacked_sevens = torch.stack(seven_tensors).float() / 255
+
+mean3 = stacked_threes.mean(0)
+ax = show_image(mean3)
+ax.figure.save_fig("mean3.png")
+
+mean7 = stacked_sevens.mean(0)
+ax = show_image(mean7)
+ax.figure.save_fig(mean7)
